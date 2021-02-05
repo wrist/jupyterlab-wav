954fcd45b39c7d31b2bf58b5f8d2d33bd8cc85c3
wrist <stoicheia1986@gmail.com>
2021-02-05 14:07:49 +0900

    Add zoom slider to AudioComponent.tsx. Modify wave plot and the background color.

--- src/AudioComponent.tsx
+++ src/AudioComponent.tsx
@@ -12,8 +12,18 @@ type AudioProps = { src?: string };
  * @returns The React component
  */
 const AudioComponent = (props: AudioProps): JSX.Element => {
+  const zoomRange = { min: 1, max: 30000, initial: 1 };
+  const fftSamplesArray = [...Array(18)].map((_, i) => Math.pow(2, i));
+
   const [isPlaying, setPlaying] = useState(false);
+  const [zoom, setZoom] = useState(zoomRange.initial);
+  //const [fftSamples, setFftSamples] = useState(fftSamplesArray[8]);
+  const fftSamples = fftSamplesArray[8];
+  // const [keypress, setKeyPress] = useState(false);
   const wavesurferRef = useRef<WaveSurfer>();
+
+  const waveColor = '#4BF2A7';
+  const bgColor = 'black';
 
   const colors = colormap({
     colormap: 'plasma',
@@ -26,7 +36,8 @@ const AudioComponent = (props: AudioProps): JSX.Element => {
     if (!wavesurferRef.current) {
       wavesurferRef.current = WaveSurfer.create({
         container: '#waveform',
-        waveColor: 'violet',
+        waveColor: waveColor,
+        backgroundColor: bgColor,
         plugins: [
           TimeLine.create({
             container: '#timeline'
@@ -35,7 +46,8 @@ const AudioComponent = (props: AudioProps): JSX.Element => {
             wavesurfer: wavesurferRef.current,
             container: '#spectrogram',
             labels: true,
-            colorMap: colors
+            colorMap: colors,
+            fftSamples: fftSamples
           })
         ]
       });
@@ -62,6 +74,50 @@ const AudioComponent = (props: AudioProps): JSX.Element => {
     }
   }, [wavesurferRef, isPlaying]);
 
+  // control zoom
+  useEffect(() => {
+    const wavesurfer = wavesurferRef.current;
+    if (wavesurfer) {
+      wavesurfer.zoom(/*pxPerSec=*/ zoom);
+    }
+  }, [wavesurferRef, zoom]);
+
+  /*
+  // space key handling
+  const downHandler = e => {
+    if (e.key === ' ') {
+      setKeyPress(true);
+    }
+  };
+  const upHandler = e => {
+    if (e.key === ' ') {
+      setKeyPress(false);
+    }
+  };
+
+  useEffect(() => {
+    // register
+    window.addEventListener('keydown', downHandler);
+    window.addEventListener('keyup', upHandler);
+
+    // cleanup
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+      window.removeEventListener('keyup', upHandler);
+    };
+  }, []);
+
+  // toggle isPlaying for every keyPress
+  useEffect(() => {
+    const wavesurfer = wavesurferRef.current;
+    if (wavesurfer) {
+      if (keypress) {
+        setPlaying(!isPlaying);
+      }
+    }
+  }, [wavesurferRef, keypress]);
+  */
+
   return (
     <div style={{ width: '100%' }}>
       <div id="timeline" />
@@ -76,6 +132,25 @@ const AudioComponent = (props: AudioProps): JSX.Element => {
         Play/Pause{' '}
       </button>
       <div> {isPlaying ? 'Playing' : 'Pause'} </div>
+      <div id="zoom">
+        <input
+          type="range"
+          value={zoom}
+          onChange={e => setZoom(Number(e.target.value))}
+          min={zoomRange.min}
+          max={zoomRange.max}
+          style={{ width: '100%' }}
+        />
+        zoom: {zoom} [pixel/sec]
+      </div>
+      {/*
+              <div id="fftSamples">
+                    FFT size:
+                    <select value={fftSamples} onChange={(e) => setFftSamples(Number(e.target.value))} >
+                      {fftSamplesArray.map((value, index) => <option value={value} >{value}</option>)}
+                    </select>
+              </div>
+              */}
     </div>
   );
 };
